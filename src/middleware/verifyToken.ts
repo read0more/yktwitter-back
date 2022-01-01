4ef5fec78dd43c3d267341fb18eded006611af10
ykpark <read0more@gmail.com>
2022-01-01 21:46:16 +0900

    verifyToken 리팩토링

--- src/middleware/verifyToken.ts
+++ src/middleware/verifyToken.ts
@@ -4,20 +4,13 @@ import { ParamsDictionary } from "express-serve-static-core";
 import { ParsedQs } from "qs";
 import { TokenInterface } from "../service/AuthService";
 
-export default function verifyToken(
-  req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
-  res: Response<any, Record<string, any>>,
-  next: NextFunction
-): void {
-  const bearerToken = req.headers?.authorization?.split(" ")?.[1];
-
-  if (bearerToken) {
+export default function verifyToken(token: string): void {
+  if (token) {
     const customer = jwt.verify(
-      bearerToken as string,
+      token,
       process.env.PASSWORD_SALT as string
     ) as TokenInterface;
     global.customer = customer;
-    next();
   } else {
     throw Error("bearer token is null.");
   }
